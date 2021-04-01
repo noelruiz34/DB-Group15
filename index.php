f22ba772742501f292d15f235f162da1b51cf62c
noel <noel@example.com>
2021-04-01 10:10:45 -0500

    testing

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+      echo "id: " . $row["student_id"]. " - Name: " . $row["name"]. " " . $row["major"]. "<br>";
     }
   } else {
     echo "0 results";
