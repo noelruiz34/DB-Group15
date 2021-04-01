162de8c6a6a68438c6aa6ee2457c21ba1e77e18e
noel <noel@example.com>
2021-04-01 12:05:18 -0500

    Tryna see some

---
+++ index2.php
@@ -0,0 +1,28 @@
+<?php
+
+$dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
+$dbUser = "admin";
+$dbPass = "12345678";
+$dbName = "Point_of_Sale";
+
+$connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName) or die("Unable to Connect to '$dbServername'");
+// mysqli_select_db($connect, $dbName) or die("Could not open the db '$dbName'");
+if($connect->connect_error) {
+    die('Bad connection'. $connect->connect_error);
+}
+
+// $test_query = "SHOW TABLES FROM $dbName";
+// $result = mysqli_query($test_query);
+
+// $tblCnt = 0;
+// while($tbl = mysqli_fetch_array($result)) {
+//   $tblCnt++;
+//   #echo $tbl[0]."<br />\n";
+// }
+
+// if (!$tblCnt) {
+//   echo "There are no tables<br />\n";
+// } else {
+//   echo "There are $tblCnt tables<br />\n";
+// }
+
