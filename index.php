5472869f43229906d2b5edbbf609cb6fcaa37378
noel <noel@example.com>
2021-04-01 10:12:55 -0500

    testing

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com"];
+$cleardb_username = $cleardb_url["admin"];
+$cleardb_password = $cleardb_url["12345678"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
