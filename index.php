1104b4620a47c5438b4fb69cb2fcb56adf366e29
noel <noel@example.com>
2021-04-01 13:18:19 -0500

    Tryna see some

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php include_once("home.html"); ?>
+<?php 
+// include_once("home.html"); 
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My website</title>
+</head>
+<body>
+
+	<a href="logout.php">Logout</a>
+	<h1>This is the index page</h1>
+
+	<br>
+	Hello, <?php echo $user_data['user_name']; ?>
+</body>
+</html>
