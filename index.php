c686d0c215037389feceb17de1c00f125d29070f
noel <noel@example.com>
2021-04-01 15:24:07 -0500

    Tryna see some

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php 
-
-$dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
-$dbUser = "admin";
-$dbPass = "12345678";
-$dbName = "Point_of_Sale";
-
-$connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName) or die("Unable to Connect to '$dbServername'");
-
-if($connect->connect_error) {
-    die('Bad connection'. $connect->connect_error);
-}
-
-$result = $connect->query("select * from student");
-$results = $result->fetch_all();
-
+include 'db.php';
 
 if(isset($_POST['btn-jack'])){
     $result = $connect->query("select * from student");
@@ -24,7 +10,9 @@ if(isset($_POST['btn-jack'])){
     }
     echo "</table>";
 }
+
 ?>
+
 <!DOCTYPE html>
 
 <script>
@@ -46,6 +34,7 @@ if(isset($_POST['btn-jack'])){
     </center>
     
 	<br>
+    
 	<?php print_r($results);?>
 </body>
 </html>
