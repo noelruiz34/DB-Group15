247552dc190fab59fd50f8b0b0bbeebdf50db45f
noel <noel@example.com>
2021-04-01 10:40:26 -0500

    testing

--- index.php
+++ index.php
@@ -17,14 +17,16 @@ if ($conn->connect_error) {
 echo "Hello world"
 
 $sql = "SELECT * from student;"
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
+
+echo $sql
+// $result = $conn->query($sql);
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     while($row = $result->fetch_assoc()) {
+//       echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+//     }
+//   } else {
+//     echo "0 results";
+//   }
 
 ?>
