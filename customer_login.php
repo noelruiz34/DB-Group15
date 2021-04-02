a78f3f6ce3ce5ea34e63d27a3a4828fb62255d37
Bryan Nguyen <bryanthenguyener@gmail.com>
2021-04-01 21:32:06 -0500

    customer_log.php file added

---
+++ customer_login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>My website</title>
+</head>
+<body>
+    
+	<h1 style="text-align:center;">Login</h1>
+
+    <center style="margin-top: 2%">
+        <form>
+            <label for="username">Email:</label>
+            <input type="text" name="username" id="username">
+        </form>
+        <form>
+            <label for="password">Password:</label>
+            <input type="text" name="password" id="password">
+        </form>
+        <a href="index.php"><button>Log in</button></a>
+        <p> Don't have an account yet? </p>
+        <a href="register.php">Create account</a>
+    </center>
+
+
+</body>
+</html>
