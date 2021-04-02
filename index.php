3e6d93a5766a4fcca90feeef6276e9d32b0ac648
Bradley Ung <bradleyung01@gmail.com>
2021-04-02 16:16:18 -0500

    cleaned up some html

--- index.php
+++ index.php
@@ -1,9 +1,57 @@
-<?php 
-include 'db.php';
-?>
-
-
 <!DOCTYPE html>
+
+<html>
+
+<head>  
+	<title>My website</title>
+</head>
+
+<body>
+    
+	<h1>Omazon</h1>
+
+    <p align="right">
+        <a href="register.php"><button>Create account</button></a>
+        <a href="customer_login.php"><button>Log in</button></a>
+    </p>
+
+   
+
+    <h1>Categories</h1>
+    <font size="+2">
+        Display all the categories from DB here somehow
+    </font>
+    <?php 
+    include 'db.php';
+    ?>
+
+    <p> display of items down here </p>
+    
+	<br>
+    <center style="margin-top: 2%">
+        <form method="post">
+            <input type="submit" name="btn-jack" value="Jack">
+
+
+        </form>
+    </center>
+    <a href="rules_status.html">Rules</a>
+    <a href="index2.php">index2 page</a>
+
+    <div id="comments">
+        <?php
+            $sql = "select * from student limit 2";
+            $result = $connect->query($sql);
+
+            echo "<table>";
+            while($kid = mysqli_fetch_array($result)){
+                echo "<tr><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
+            }
+            echo "</table>";
+        ?>
+    </div>
+    <button> Show one more </button>
+</body>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
@@ -20,51 +68,4 @@ include 'db.php';
     
 </script>
 
-<html>
-<head>  
-	<title>My website</title>
-</head>
-<body>
-    
-    <a href="rules_status.html">Rules</a>
-	<h1>Omazon</h1>
-    <a href="index2.php">index2 page</a>
-
-    <p align="right">
-        <a href="register.php"><button>Create account</button></a>
-        <a href="customer_login.php"><button>Log in</button></a>
-    </p>
-
-   
-
-    <h1>Categories</h1>
-    <font size="+2">
-        Display all the categories from DB here somehow
-    </font>
-
-    <p> display of items down here </p>
-    
-	<br>
-    <center style="margin-top: 2%">
-        <form method="post">
-            <input type="submit" name="btn-jack" value="Jack">
-
-
-        </form>
-    </center>
-
-    <div id="comments">
-        <?php
-            $sql = "select * from student limit 2";
-            $result = $connect->query($sql);
-
-            echo "<table>";
-            while($kid = mysqli_fetch_array($result)){
-                echo "<tr><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
-            }
-            echo "</table>";
-        ?>
-    </div>
-    <button> Show one more </button>
-</body>
 </html>
