cb48f70798874e89558dc9c82665a7d427de4361
noel <noel@example.com>
2021-04-02 14:30:23 -0500

    Tryna see some

--- index.php
+++ index.php
@@ -17,22 +17,6 @@ include 'db.php';
             });
          });
     });
-
-
-    function show(id) {
-        var x = document.getElementById(id);
-        collapseAll();
-
-        if (x.style.display === "none") {
-            x.style.display = "block";
-            var y = document.getElementById(`${id}`);
-            y.style.backgroundColor = "rgba(0,0, 0,.5)";
-        } else {
-            var y = document.getElementById(`R ${id}`);
-            y.style.backgroundColor = "rbga(0,0,0,.08)";
-            x.style.display = "none";
-        }
-    }
     
 </script>
 
@@ -76,7 +60,7 @@ include 'db.php';
 
             echo "<table>";
             while($kid = mysqli_fetch_array($result)){
-                echo "<tr onclick='show('".$kid['name'].")' id ='".$kid['name']."'><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
+                echo "<tr><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
             }
             echo "</table>";
         ?>
