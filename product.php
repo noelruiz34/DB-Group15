90f2efeb468411e86a423fcda9f5bf9bc138ac5b
jhaelon <jhaelonedwards@gmail.com>
2021-04-01 22:24:12 -0500

    testing query print on prodcuts page1

---
+++ product.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Products</title>
+</head>
+<body>
+    
+	<h1 style="text-align:center;">Check Out Our Products(Best Prices in Town)</h1>
+
+
+
+
+    <center style="margin-top: 2%">
+       
+    </center>
+
+
+</body>
+</html>
+
+
+<?php 
+
