df80d48f0ede9e7167675cd49c976dd929f92208
Bryan Nguyen <bryanthenguyener@gmail.com>
2021-04-03 16:37:58 -0500

    added some buttons into employe_home.php

--- employee_home.php
+++ employee_home.php
@@ -1,3 +1,27 @@
+<?php
+    $dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
+    $dbUser = "admin";
+    $dbPass = "12345678";
+    $dbName = "Point_of_Sale";
+    $connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
+    if(mysqli_connect_errno())
+    {
+        die("connection Failed! " . mysqli_connect_error());
+    }
+
+    if($_POST) {
+        $sql = "SELECT * FROM support_ticket";
+        $result = mysqli_query($connect,$sql);
+        if(!$result) {
+            die("Query Failed!");
+        }
+        while($row=mysqli_fetch_array($result)) {
+            echo $row ;
+        }
+    }
+?>
+
+
 <!DOCTYPE html> <!-- This page is for after an employee logs in -->
 <html>
 <head>
@@ -15,15 +39,31 @@
         Hello "insert login name here"!
     </font>
 
+    <?php
+        $sql = "SELECT * FROM product_category";
+        $result = mysqli_query($connect,$sql);
+        if(!$result) {
+            die("Query Failed!");
+        }
+        while($row=mysqli_fetch_array($result)) {
+            echo $row['category_name'];
+        }
+        
+    ?>
+
     <h1>Product</h1>
     <a href="add_product.php"><button>Add Product</button></a>
     <a href="update_product.php"><button>Update Product</button></a>
+    <a href="add_product_category.php"><button>Add Product Category</button></a>
+    <a href="add_product_category.php"><button>Update Product Category</button></a>
     <p><label>Search Product Update History  :</label><input type = "text" name = "product_update_history" class = "box"/><br /><br /></p>
 
     <h1>Support Tickets</h1>
-    <input type = "submit" value = " View All Tickets "/><br />
-    <a href="update_tickets.php"><button>Update Ticket</button></a>
-    <p><label>Search Support Ticket  :</label><input type = "text" name = "support_ticket" class = "box"/><br /><br /></p>
+    <form method="POST">
+        <input type = "submit" name = "view_all_tickets" value = " View All Tickets "/><br />
+        <a href="update_tickets.php"><button>Update Ticket</button></a>
+        <p><label>Search Support Ticket  :</label><input type = "text" id = "support_ticket" name = "support_ticket" class = "box"/><br /><br /></p>
+    <form>
 
     <h1>Sales</h1>
     <input type = "submit" value = " Generate Sales Report "/><br />
