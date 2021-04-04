f190c6717b1104dd8a6259f8b7674c8efc2e0626
noel <noel@example.com>
2021-04-04 13:48:01 -0500

    testing email functions

--- sendemail.php
+++ sendemail.php
@@ -3,9 +3,13 @@ $to = 'noelruiz34@gmail.com';
 $subject = 'Marriage Proposal';
 $message = 'Hi Jane, will you marry me?'; 
 $from = 'pointofsale.group15@gmail.com';
+$headers = array("From: from@example.com",
+    "Reply-To: replyto@example.com",
+    "X-Mailer: PHP/" . PHP_VERSION
+);
  
 // Sending email
-if(mail($to, $subject, $message)){
+if(mail($to, $subject, $message, $headers)){
     echo 'Your mail has been sent successfully.';
 } else{
     echo 'Unable to send email. Please try again.';
