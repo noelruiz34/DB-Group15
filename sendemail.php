8c1bde97e426de70dfd227141124da1a6ad5cef1
noel <noel@example.com>
2021-04-04 13:43:10 -0500

    testing email functions

--- sendemail.php
+++ sendemail.php
@@ -1,31 +1,10 @@
 <?php
-// PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer; 
-use PHPMailer\PHPMailer\Exception;
-// Base files 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-// create object of PHPMailer class with boolean parameter which sets/unsets exception.
-$mail = new PHPMailer(true);                              
-try {
-    $mail->isSMTP(); // using SMTP protocol                                     
-    $mail->Host = 'smtp.gmail.com'; // SMTP host as gmail 
-    $mail->SMTPAuth = true;  // enable smtp authentication                             
-    $mail->Username = 'pointofsale.group15@gmail.com';  // sender gmail host              
-    $mail->Password = 'POSgroup!'; // sender gmail host password                          
-    $mail->SMTPSecure = 'tls';  // for encrypted connection                           
-    $mail->Port = 587;   // port for SMTP     
+    $to      = 'noelruiz34@gmail.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com'       . "\r\n" .
+                 'Reply-To: webmaster@example.com' . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
 
-    $mail->setFrom('pointofsale.group15@gmail.com', "Sender"); // sender's email and name
-    $mail->addAddress('noelruiz34@gmail.com', "Receiver");  // receiver's email and name
-
-    $mail->Subject = 'Test subject';
-    $mail->Body    = 'Test body';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) { // handle error.
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-}
+    mail($to, $subject, $message, $headers);
 ?>
