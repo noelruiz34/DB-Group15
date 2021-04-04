4054a6853ba8e19abba577d172eb85a038b45283
jhaelon <jhaelonedwards@gmail.com>
2021-04-04 18:27:13 -0500

    testing query print on prodcuts page6

--- product.php
+++ product.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     
-	<h1 style="text-align:center;">Check Out Our Products(Best Prices in Town)</h1>
+	<h1 style="text-align:center;">Check Out Our Products(Best Prices Around)</h1>
 
 
 
@@ -27,16 +27,13 @@ include 'db.php';
 
     $result = $connect->query("select category_name from product_category");
 
-    echo "<table>";
-    while($kid = mysqli_fetch_array($result)){
-        
-        echo "<tr><td>". $row['category_name']. "</td></tr>";
-        
-     // <button>Create account</button></a>
-       // <button type="button" onclick="alert('Hello world!')">Click Me!</button>;
-        
-    }
-    echo "</table>";
+    
+        echo "<table>";
+        while($kid = mysqli_fetch_array($result)){
+            echo "<tr><td>". $row['category_name']. "</td></tr>";
+        }
+        echo "</table>";
+    //
 
 
 ?>
