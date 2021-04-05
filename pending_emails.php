7201b25878e595dd80c928739ece5f2552829894
Bryan Nguyen <bryanthenguyener@gmail.com>
2021-04-05 12:14:13 -0500

    implemented display of pending emails

---
+++ pending_emails.php
@@ -0,0 +1,26 @@
+<?php 
+    $dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
+    $dbUser = "admin";
+    $dbPass = "12345678";
+    $dbName = "Point_of_Sale";
+    $connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
+    if(mysqli_connect_errno())
+    {
+        die("connection Failed! " . mysqli_connect_error());
+    }
+    $sql = "SELECT * FROM pending_email";
+    $result = mysqli_query($connect,$sql);
+    if(!$result) {
+        die("Query Failed!");
+    }
+    /*while($row=mysqli_fetch_array($result)) {
+        echo $row['o_id'] ;
+    } */
+    echo "<table>";
+        echo "<tr><td> Email ID </td><td> Recipient Email </td><td> Subject </td></tr>";
+        while($row=mysqli_fetch_array($result)){
+            echo "<tr><td>". $row['pending_email_id']. "</td><td>" . $row ['p_email'] . "</td><td>" . $row ['email_subject'] . "</td></tr>";
+        }
+    echo "</table>";
+
+?>
