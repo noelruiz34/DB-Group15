ae4881f105bed17bfc0758d4fc31e5f68878b60a
noel <noel@example.com>
2021-04-04 19:25:45 -0500

    testing order summary

--- retrieve_order.php
+++ retrieve_order.php
@@ -4,12 +4,12 @@ include "db.php";
 
 $order = $_POST["search"];
 
-$sql = "select * from order where o_id = $order";
-$result = $connect->query($sql);
+// $sql = "select * from order where o_id = $order";
+// $result = $connect->query($sql);
 
 echo "<div>";
 
-echo $result
+echo $order
 // while($kid = mysqli_fetch_array($result)){
 //     echo "<tr><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
 // }
