f8033b55664e40955a12417255877c8541f4cc21
noel <noel@example.com>
2021-04-04 19:51:38 -0500

    testing order summary

--- retrieve_order.php
+++ retrieve_order.php
@@ -1,16 +1,32 @@
 <?php
 
-$order = $_POST["search"];
+include "db.php";
 
-// $sql = "select * from order where o_id = $order";
-// $result = $connect->query($sql);
+// $order = $_POST["search"];
 
-echo "<div>";
+// // $sql = "select * from order where o_id = $order";
+// // $result = $connect->query($sql);
 
-echo $order
-// while($kid = mysqli_fetch_array($result)){
-//     echo "<tr><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
-// }
-echo "</div>";
+// echo "<div>";
+
+// echo $order
+// // while($kid = mysqli_fetch_array($result)){
+// //     echo "<tr><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
+// // }
+// echo "</div>";
+
+
+if(isset($_POST['search'])){
+
+    $order = $_POST["search"];
+    // $result = $connect->query("select * from student");
+
+    echo "<table>";
+    echo $order
+    // while($kid = mysqli_fetch_array($result)){
+    //     echo "<tr><td>". $kid['name']. "</td> <td>". $kid['major']. "</td></tr>";
+    // }
+    echo "</table>";
+}
 
 ?>
