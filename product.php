4bb87fe2b9cb7cbfb5385d8da1a31f2b4118e07d
jhaelon <jhaelonedwards@gmail.com>
2021-04-04 23:13:15 -0500

    testing query print on prodcuts page8

--- product.php
+++ product.php
@@ -35,33 +35,11 @@ include 'db.php';
     
         //echo "<table>";
         while($row = mysqli_fetch_array($result)){
-            //echo "<tr><td>". $row['category_name']. "</td></tr>";
-
-            //print_r($row['category_name']);
-           /* function test()
-            {
-                print_r("hello");
-            }*/
-            
-           // echo "<button>".$row['category_name']. "</button>";
-
-            //echo "<input type='submit' name=".$row['category_name']." value = ".$row['category_name']." id ='submit'>";
-            //echo "<input type='button' onclick='test()' name =".$row['category_name']." value = ".$row['category_name']." />";
-/*
-            echo '<form method="post">';
-            echo '<u>'.$row_all["name"].'</u>';
-
-            echo '<br>';
-
-            echo '<button name='.$row['category_name'].' value='.$row['category_name'].' type="submit">'.$row['category_name'].'</button>';
-
-            echo '<hr>';
-        echo '</form>';
-*/
+        echo "<tr><td>". $row['category_name']. "</td></tr><br>";
 
 echo "<form method='post' name='proddisp' action=''>";
  
-echo'<input type="submit" id="button" name="proddisp" value =' .$row['category_name']. ' >';
+echo'<input type="submit" id="button" name="proddisp" value ='.$row['category_name'].'>';
 echo '<input type="hidden" name="disp_this" value="'.$row['category_name'].'">';
     echo "</form>";
 
@@ -73,8 +51,7 @@ echo '<input type="hidden" name="disp_this" value="'.$row['category_name'].'">';
    
     if(isset($_POST["proddisp"]))
             {
-                $id_targ = $_GET['id'];
-                echo($id_targ);
+               
 
             /*echo"print_r('Hello')";
             print_r('hello');
