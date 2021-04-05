a05d4ee8f6c6a120316accd10044bbc7da9f11ac
noel <noel@example.com>
2021-04-04 20:10:33 -0500

    testing order summary

--- order_summary.php
+++ order_summary.php
@@ -9,6 +9,9 @@
 <html>
 <head>
 
+<a href="index.php">Back to Home</a>
+
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
