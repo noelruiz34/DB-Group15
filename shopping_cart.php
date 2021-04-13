8534b15bd74dcb45de22c6f4dc61ecec08248e26
Bryan Nguyen <bryanthenguyener@gmail.com>
2021-04-12 20:05:30 -0500

    my cart functionallity

--- shopping_cart.php
+++ shopping_cart.php
@@ -1,29 +1,33 @@
 <?php
-session_start();
-$status="";
-if (isset($_POST['action']) && $_POST['action']=="remove"){
-if(!empty($_SESSION["shopping_cart"])) {
-    foreach($_SESSION["shopping_cart"] as $key => $value) {
-      if($_POST["code"] == $key){
-      unset($_SESSION["shopping_cart"][$key]);
-      $status = "<div class='box' style='color:red;'>
-      Product is removed from your cart!</div>";
-      }
-      if(empty($_SESSION["shopping_cart"]))
-      unset($_SESSION["shopping_cart"]);
-      } 
-}
-}
- 
-if (isset($_POST['action']) && $_POST['action']=="change"){
-  foreach($_SESSION["shopping_cart"] as &$value){
-    if($value['upc'] === $_POST["upc"]){
-        $value['p_quantity'] = $_POST["p_quantity"];
-        break; // Stop the loop after we've found the product
+  $dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
+  $dbUser = "admin";
+  $dbPass = "12345678";
+  $dbName = "Point_of_Sale";
+  $connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
+  if(mysqli_connect_errno())
+  {
+      die("connection Failed! " . mysqli_connect_error());
+  }
+  session_start();
+  if(!isset($_SESSION['customer'])) // If session is not set then redirect to Login Page
+    {
+      header("Location:customer-login.php");  
     }
-}
-   
-}
+
+  $customer_id = $_SESSION['customer'];
+  
+  $cart_sql = "SELECT * FROM shopping_cart WHERE customer_id=$customer_id";
+  $cart_results = mysqli_query($connect, $cart_sql);
+  echo "<table>";
+  echo "<tr><td> UPC </td><td> Quantity </td></tr>";
+  while($row=mysqli_fetch_array($cart_results)) {
+    echo "<tr>
+    <td>" . $row['upc'] . "</td>
+    <td>" . $row['cart_quantity'] . "</td>
+    </tr>";
+  }
+  echo "</table>";
+
 ?>
 <div class="cart">
 <?php
