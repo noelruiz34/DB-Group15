4956381d052b54b013668a9a698efddb4d4afe00
kochbigge@gmail.com <randy@RJ-PC.localdomain>
2021-04-14 08:28:52 -0500

    shopping cart

--- shopping_cart.php
+++ shopping_cart.php
@@ -3,10 +3,12 @@
   </body>
   
 <?php
+
   $dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
   $dbUser = "admin";
   $dbPass = "12345678";
   $dbName = "Point_of_Sale";
+  
   $connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
   if(mysqli_connect_errno())
   {
@@ -17,13 +19,15 @@
     {
       header("Location:customer_login.php");  
     }
-
-  $customer_id = $_SESSION['customer'];
+    $customer_id = $_SESSION['customer'];
+  CartUpdate($customer_id, $connect);
   
+  function CartUpdate($custVar,$connectID)
+  {
   
-  $cart_sql = "SELECT *, product.p_name, product.p_price FROM shopping_cart INNER JOIN product ON shopping_cart.upc=product.upc WHERE customer_id=$customer_id";
+  $cart_sql = "SELECT *, product.p_name, product.p_price FROM shopping_cart INNER JOIN product ON shopping_cart.upc=product.upc WHERE customer_id=$custVar";
   
-  $cart_results = mysqli_query($connect, $cart_sql);
+  $cart_results = mysqli_query($connectID, $cart_sql);
   
   echo "<table>";
   echo "<tr><td> Product Name </td><td> Quantity </td><td> Price </td></tr> ";
@@ -48,7 +52,8 @@
   $cart_total
   </tr>
   ";
-  
-  
-
+}
 ?>
+
+
+<a href="checkout.php">Proceed To Checkout</a>
