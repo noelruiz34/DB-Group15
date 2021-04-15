5cce21e070e45dd92ea072db2d9f45fc50c01fda
noel <noel@example.com>
2021-04-15 14:11:25 -0500

    updating order lookup

--- order-summary.php
+++ order-summary.php
@@ -144,10 +144,9 @@ td {
     if(isset($_POST['status']))
     {
       $status = $_POST['status'];
-      $order = $_POST['search'];
+      $order = $_SESSION['order'];
 
       echo $status;
-      echo $_SESSION['order'];
       $update = $connect->query("update Point_of_Sale.order set o_status = '$status' where o_id = $order");
     }
     ?>
