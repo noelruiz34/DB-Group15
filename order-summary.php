240c23459c2e10ea8d92af2ab044054bfdc3b9bb
noel <noel@example.com>
2021-04-15 14:03:44 -0500

    updating order lookup

--- order-summary.php
+++ order-summary.php
@@ -142,7 +142,7 @@ td {
     if(isset($_POST['status']))
     {
       $status = $_POST['status'];
-      $order = $_POST['search']
+      $order = $_POST['search'];
 
       echo $status;
       echo "update Point_of_Sale.order set o_status = '$status' where o_id = $order";
