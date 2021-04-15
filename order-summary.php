36974c2b714b6edeb17f2f97a74158801deae6e9
noel <noel@example.com>
2021-04-15 10:52:32 -0500

    updating order lookup

--- order-summary.php
+++ order-summary.php
@@ -129,11 +129,6 @@ td {
             echo "<tr><td>".$item['upc']."</td><td>".$item['quantity_ordered']."</td><td>".$item['p_price']."</td></tr>";
         }
         echo "</table>";
-        echo " <a style={{ fontSize: \"150%\", marginRight: \"1%\" }}>Price:</a>
-                <input type=\"radio\" name=\"status\" id=\"p1\" /> $
-                <input type=\"radio\" name=\"status\" id=\"p2\" /> $ $
-                <input type=\"radio\" name=\"status\" id=\"p3\" /> $ $ $
-                <input type=\"radio\" name=\"status\" id=\"p4\" /> $ $ $ $"
     }
     ?>
 
