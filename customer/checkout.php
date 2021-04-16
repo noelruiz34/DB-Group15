7a3abe78c6a295a97837d2c919b5529016495a87
kochbigge@gmail.com <randy@RJ-PC.localdomain>
2021-04-16 13:05:48 -0500

    checkout page bare

--- customer/checkout.php
+++ customer/checkout.php
@@ -2,24 +2,27 @@
 <body>
   <a href="/index.php">Return to homepage</a>
   </body>
-  <?php>
-$dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
-$dbUser = "admin";
-$dbPass = "12345678";
-$dbName = "Point_of_Sale";
   
-$connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
-if(mysqli_connect_errno())
-{
-    die("connection Failed! " . mysqli_connect_error());
-}
-session_start();
-if(!isset($_SESSION['customer'])) // If session is not set then redirect to Login Page
-{
-    header("Location:customer-login.php");  
-}
-$customer_id = $_SESSION['customer'];
-displayCart($customer_id,$connect);
+<?php
+
+  $dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
+  $dbUser = "admin";
+  $dbPass = "12345678";
+  $dbName = "Point_of_Sale";
+  
+  $connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
+  if(mysqli_connect_errno())
+  {
+      die("connection Failed! " . mysqli_connect_error());
+  }
+  session_start();
+  if(!isset($_SESSION['customer'])) // If session is not set then redirect to Login Page
+    {
+      header("Location:/customer/customer-login.php");  
+    }
+    $customer_id = $_SESSION['customer'];
+  
+    displayCart($customer_id, $connect);
 function displayCart($cust_id, $conn)
   {
     ob_start();
@@ -38,51 +41,21 @@ function displayCart($cust_id, $conn)
       $cart_price = $cart_qty * $cart_p;
       echo "<tr>
       <td>" . $row['p_name'] . "</td>
+      <td>" . $row['cart_quantity'] . "</td>
       ";
+         
       echo"
-      <form method='post' action=''>
-      <input type = 'hidden' name = 'remove_upc' value= ".$row['upc'].">
-      <input type = 'hidden' name = 'iquant' value= ".$row['p_quantity'].">
-      ";
-      echo "<td>
-      <select name = qp>
-      ";
-      
-      for ($h = $row['cart_quantity']; $h >= 1; $h--) 
-        {
-          echo '<option value='.$h.'>'.$h.'</option>';
-        }
-      echo '</select>';
-      echo "<input type = 'submit' name = 'remove_from_cart' value = 'Remove'/><br />
-            </form>
-            ";
-            
-      echo"
-      <td><form method='post' action=''>
-      <input type = 'hidden' name = 'remove_upc' value= ".$row['upc'].">
-      <input type = 'hidden' name = 'add_upc' value= ".$row['upc'].">
-      <input type = 'hidden' name = 'iquant' value= ".$row['p_quantity'].">
       <td>" . $cart_price . "</td>
       </tr>";
       echo "<td>
       <select name = qp>
       ";
-
-      for ($h = 1; $h <=($row['p_quantity'] - $row['cart_quantity']); $h++) 
-        {
-          echo '<option value='.$h.'>'.$h.'</option>';
-        }
-      echo '</select>';
-      echo "<input type = 'submit' name = 'add_more_to_cart' value = 'Add More To Cart'/><br />
-            </form>
-            ";
-      
       $cart_total = $cart_total + $cart_price;
     }
     echo "</table>";
     echo "<br>
       <tr>
-      <td> Total </td>
+      <td> Total:  </td>
       <td>  </td>
       $$cart_total
     </tr>
