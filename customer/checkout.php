3f323208a514d6c6a019e1cbd4e2c448680a3683
kochbigge@gmail.com <randy@RJ-PC.localdomain>
2021-04-16 12:59:31 -0500

    checkout page bare

--- customer/checkout.php
+++ customer/checkout.php
@@ -16,7 +16,7 @@ if(mysqli_connect_errno())
 session_start();
 if(!isset($_SESSION['customer'])) // If session is not set then redirect to Login Page
 {
-    header("Location:/customer/customer-login.php");  
+    header("Location:customer-login.php");  
 }
 $customer_id = $_SESSION['customer'];
 displayCart($customer_id,$connect);
@@ -38,21 +38,51 @@ function displayCart($cust_id, $conn)
       $cart_price = $cart_qty * $cart_p;
       echo "<tr>
       <td>" . $row['p_name'] . "</td>
-      <td>" . $row['cart_quantity'] . "</td>
       ";
-         
       echo"
+      <form method='post' action=''>
+      <input type = 'hidden' name = 'remove_upc' value= ".$row['upc'].">
+      <input type = 'hidden' name = 'iquant' value= ".$row['p_quantity'].">
+      ";
+      echo "<td>
+      <select name = qp>
+      ";
+      
+      for ($h = $row['cart_quantity']; $h >= 1; $h--) 
+        {
+          echo '<option value='.$h.'>'.$h.'</option>';
+        }
+      echo '</select>';
+      echo "<input type = 'submit' name = 'remove_from_cart' value = 'Remove'/><br />
+            </form>
+            ";
+            
+      echo"
+      <td><form method='post' action=''>
+      <input type = 'hidden' name = 'remove_upc' value= ".$row['upc'].">
+      <input type = 'hidden' name = 'add_upc' value= ".$row['upc'].">
+      <input type = 'hidden' name = 'iquant' value= ".$row['p_quantity'].">
       <td>" . $cart_price . "</td>
       </tr>";
       echo "<td>
       <select name = qp>
       ";
+
+      for ($h = 1; $h <=($row['p_quantity'] - $row['cart_quantity']); $h++) 
+        {
+          echo '<option value='.$h.'>'.$h.'</option>';
+        }
+      echo '</select>';
+      echo "<input type = 'submit' name = 'add_more_to_cart' value = 'Add More To Cart'/><br />
+            </form>
+            ";
+      
       $cart_total = $cart_total + $cart_price;
     }
     echo "</table>";
     echo "<br>
       <tr>
-      <td> Total:  </td>
+      <td> Total </td>
       <td>  </td>
       $$cart_total
     </tr>
