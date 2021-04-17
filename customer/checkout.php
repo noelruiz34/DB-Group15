9660faa45b5d58e60ef06cf9e06c2b41c2ced91d
kochbigge@gmail.com <randy@RJ-PC.localdomain>
2021-04-16 19:08:15 -0500

    checkout page

--- customer/checkout.php
+++ customer/checkout.php
@@ -64,6 +64,7 @@ function displayCart($cust_id, $conn)
 ?>
 <input type = 'submit' name = 'pay' value= 'Pay'/>
 <?php
+#a customer can only be fully registered if they have a billing address and a shipping address and billing info, thus we can just proceed to payment
 if ($_PAY['pay']=='Pay')
 {
   $cart_sql = "SELECT *, product.p_name, product.p_price FROM shopping_cart INNER JOIN product ON shopping_cart.upc=product.upc WHERE customer_id=$customer_id";
@@ -71,28 +72,59 @@ if ($_PAY['pay']=='Pay')
   $cart_results = mysqli_query($connect, $cart_sql);
   #run a loop to get the upc of cart items, then reduce product database p_quantity by that amount
   #then add a new order with the appropriate o_status
+  $cart_price = 0;
   while($row=mysqli_fetch_array($cart_results)) 
   {
     $cart_qty =  floatval($row['cart_quantity']);
+    $cart_p = floatval($row['p_price']);
     $cart_price = $cart_qty * $cart_p;
     
     $cart_total = $cart_total + $cart_price;
+
+    
   }
+  $order_sql = "SELECT * FROM order";
+  $order_results = mysqli_query($connect,$order_sql);
+  $new_order_num = 0;
+  #add this order to a new order number
+  #loop through all order numbers and generate a new one?
+  while($row = mysqli_fetch_array($order_results))
+  {
+    $new_order_num = $new_order_num + 1;
+    #after this loop, the new_order_num variable will equal the greatest number order number in the DB
+    #we want 1 number past this
+  }
+  
+  $new_order_num = $new_order_num + 1;
+  $timestamp = date('Y-m-d H:i:s');
+
+  $whole_order = $connection->prepare('INSERT INTO order VALUES (:o_id, :customer_id, :otime, :o_status)');
+  if ($whole_order)
+  {
+    $result = $whole_order->execute ([
+      'o_id' => $new_order_num,
+      'customer_id' => $customer_id,
+      'otime' => $timestamp,
+      'o_status' => "Processing"
+    ]);
+    if ($whole_order) {
+      $_SESSION['messages'][] = 'Order placed! Thank you for shopping with Omazon!';
+    }
+    $cart_sql = "SELECT * FROM shopping_cart WHERE customer_id=$cust_id";
+    $cart_results = mysqli_query($conn, $cart_sql);
+    
+    while($row=mysqli_fetch_array($cart_results)) 
+    {
+      $deleteCart = "DELETE FROM shopping_cart WHERE customer_id = $cust_id";
+      mysqli_query($conn, $deleteCart);
+
+      #DELETE FROM `Point_of_Sale`.`shopping_cart` WHERE (`customer_id` = '1') and (`upc` = '1');
+      
+    }
+
+  }
+
 }
     
     
-  
-      
-    
-  
-  
- 
-
-  
-    
-  
-
-
-
-
 
