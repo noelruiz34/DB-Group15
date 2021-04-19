e48bd15b423315d484b52297b9bbc89817040c58
noel <noel@example.com>
2021-04-18 20:48:11 -0500

    adding item name to order lookup

--- employee/order-summary.php
+++ employee/order-summary.php
@@ -97,7 +97,7 @@ td {
 
         $_SESSION['order'] = $order;
 
-        echo "<table id=\"orderInfo\">";
+        echo "<table id=\"orderInfo\" style=\"margin-top: -2%;>";
         while($order_info = mysqli_fetch_array($result))
         {
             echo "<tr><td> Order ID: ".$order_info['o_id']."</td></tr>";
