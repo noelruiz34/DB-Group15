512f1a01624d05f5e6b0940e79123c0130df23c9
noel <noel@example.com>
2021-04-18 19:36:29 -0500

    fixing DB call for employee name

--- employee/employee-portal.php
+++ employee/employee-portal.php
@@ -74,7 +74,6 @@
     <font size="+1"> <!-- Not sure if this is necessary -->
        <?php
             $result = $connect->query("select * from Point_of_Sale.employee where ssn=$employee_id");
-            echo $result;
             while($row = mysqli_fetch_array($result))
             {      
                 echo "Hello, ".$row['f_name']." ".$row['l_name']."!";
