b7beaa93551b5e9589edfc76dfcb49ea11105c4a
noel <noel@example.com>
2021-04-20 12:44:11 -0500

    formatting sales report

--- employee/sales.php
+++ employee/sales.php
@@ -305,10 +305,11 @@ tr:nth-child(even) {
         <select id="method" name = "view_method">
                 <option value="orders_and_returns"> Orders and Returns</option>
                 <option value="product_view"> Products and Categories</option>
-        </select> 
+        </select> <br>
         Start Date: <input type='date' id='start' name='sales_start' required/>
         End Date: <input type='date' id='end' name='sales_end' required/>
         <input type = "submit" name = "generate_report" value = "Generate Report"/>
+        <br>
     </form>
     
     <?php 
