b75c5df1e5d082fe90bb272697e4a87823b0c7ba
noel <noel@example.com>
2021-04-22 11:08:31 -0500

    removing redundant code

--- employee/product-changes-report.php
+++ employee/product-changes-report.php
@@ -1,13 +1,5 @@
 <?php
-    $dbServername = "database-1.cgnsxr0vmecq.us-east-2.rds.amazonaws.com";
-    $dbUser = "admin";
-    $dbPass = "12345678";
-    $dbName = "Point_of_Sale";
-    $connect = mysqli_connect($dbServername, $dbUser, $dbPass, $dbName);
-    if(mysqli_connect_errno())
-    {
-        die("connection Failed! " . mysqli_connect_error());
-    }
+    include '../db.php';
     session_start();
     if(!isset($_SESSION['employee'])) // If session is not set then redirect to Login Page
        {
